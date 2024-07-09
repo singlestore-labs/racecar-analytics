a0357713a58487f615031f15c9721909acf7470f
Bharat Kathi <bkathi@singlestore.com>
2024-07-09 16:21:20 -0700

    add db connection

--- ingest/db.go
+++ ingest/db.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var DatabaseHost = ""
+var DatabasePort = ""
+var DatabaseUser = ""
+var DatabasePassword = ""
+var DatabaseName = ""
+
+func ConnectDB() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=UTC", DatabaseUser, DatabasePassword, DatabaseHost, DatabasePort, DatabaseName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	DB = db
+	log.Println("Connected to database")
+	err = db.AutoMigrate(ECU{}, Battery{})
+	if err != nil {
+		log.Fatalf("AutoMigration failed: %v", err)
+	}
+	log.Println("AutoMigration completed")
+}
