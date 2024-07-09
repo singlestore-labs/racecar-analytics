8af421a8ec825ded063d0a02e9046389f5467621
Bharat Kathi <bkathi@Bharats-SingleStore-MacBook-Pro.local>
2024-07-09 13:50:36 -0700

    mqtt connection

--- ingest/main.go
+++ ingest/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	ConnectMQTT()
+}
+
+var Client mqtt.Client
+
+var MQTTHost = "localhost"
+var MQTTPort = "1883"
+
+func ConnectMQTT() {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%s", MQTTHost, MQTTPort))
+	opts.OnConnect = connectHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	Client = client
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("[MQ] Connected to MQTT broker!")
 }
